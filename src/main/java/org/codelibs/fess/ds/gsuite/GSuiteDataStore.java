4fa7928a6b39885f121285e209c63bbe7734bfa7
Ryo Kuramoto <kuramoto@n2sm.net>
2018-09-26 10:49:44 +0900

    add getDriveService

--- src/main/java/org/codelibs/fess/ds/gsuite/GSuiteDataStore.java
+++ src/main/java/org/codelibs/fess/ds/gsuite/GSuiteDataStore.java
@@ -15,17 +15,53 @@
  */
 package org.codelibs.fess.ds.gsuite;
 
-import java.util.Map;
-
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpContent;
+import com.google.api.client.http.HttpResponse;
+import com.google.api.client.http.UrlEncodedContent;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.GenericData;
+import com.google.api.client.util.PemReader;
+import com.google.api.client.util.SecurityUtils;
+import com.google.api.services.drive.Drive;
 import org.codelibs.fess.ds.AbstractDataStore;
 import org.codelibs.fess.ds.callback.IndexUpdateCallback;
 import org.codelibs.fess.es.config.exentity.DataConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Map;
+
 public class GSuiteDataStore extends AbstractDataStore {
 
     private static final Logger logger = LoggerFactory.getLogger(GSuiteDataStore.class);
+
+    // parameters
+    private static final String PROJECT_ID_PARAM = "project_id";
+    private static final String PRIVATE_KEY_PARAM = "private_key";
+    private static final String PRIVATE_KEY_ID_PARAM = "private_key_id";
+    private static final String CLIENT_EMAIL_PARAM = "client_email";
+
+    // scripts
+    protected static final String MESSAGE = "message";
 
     protected String getName() {
         return "G Suite";
@@ -34,6 +70,116 @@ public class GSuiteDataStore extends AbstractDataStore {
     @Override
     protected void storeData(final DataConfig dataConfig, final IndexUpdateCallback callback, final Map<String, String> paramMap,
             final Map<String, String> scriptMap, final Map<String, Object> defaultDataMap) {
+
+        final String projectId = paramMap.getOrDefault(PROJECT_ID_PARAM, "");
+        final String privateKeyPem = paramMap.getOrDefault(PRIVATE_KEY_PARAM, "");
+        final String privateKeyId = paramMap.getOrDefault(PRIVATE_KEY_ID_PARAM, "");
+        final String clientEmail = paramMap.getOrDefault(CLIENT_EMAIL_PARAM, "");
+
+        if (projectId.isEmpty() || privateKeyPem.isEmpty() || privateKeyId.isEmpty() || clientEmail.isEmpty()) {
+            logger.warn("parameter '" + //
+                    PROJECT_ID_PARAM + "', '" + //
+                    PRIVATE_KEY_PARAM + "', '" + //
+                    PRIVATE_KEY_ID_PARAM + "', '" + //
+                    CLIENT_EMAIL_PARAM + "' is required");
+            return;
+        }
+
+        final PrivateKey privateKey;
+        try {
+            privateKey = getPrivateKey(privateKeyPem);
+        } catch (IOException | NoSuchAlgorithmException | InvalidKeySpecException e) {
+            logger.warn("Failed to get '" + PRIVATE_KEY_PARAM + "'", e);
+            return;
+        }
+
+        final Drive drive;
+        try {
+            drive = getDriveService(projectId, privateKey, privateKeyId, clientEmail);
+        } catch (final IOException e) {
+            logger.warn("Failed to get Drive Service", e);
+            return;
+        }
+
+    }
+
+    protected static PrivateKey getPrivateKey(final String privateKeyPem)
+            throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+        final Reader reader = new StringReader(privateKeyPem);
+        final PemReader.Section section = PemReader.readFirstSectionAndClose(reader, "PRIVATE KEY");
+        final byte[] bytes = section.getBase64DecodedBytes();
+        final PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(bytes);
+        final KeyFactory keyFactory = SecurityUtils.getRsaKeyFactory();
+        return keyFactory.generatePrivate(keySpec);
+    }
+
+    protected static Drive getDriveService(final String projectId, final PrivateKey privateKey, final String privateKeyId,
+            final String clientEmail) throws IOException {
+        final GoogleCredential credential = new GoogleCredential.Builder() //
+                .setTransport(new NetHttpTransport()).setJsonFactory(new JacksonFactory()).setServiceAccountId(clientEmail)
+                .setServiceAccountScopes(Collections.emptyList()).setServiceAccountPrivateKey(privateKey)
+                .setServiceAccountPrivateKeyId(privateKeyId).setTokenServerEncodedUrl("https://oauth2.googleapis.com/token")
+                .setServiceAccountProjectId(projectId).build();
+
+        final long now = System.currentTimeMillis();
+
+        final String jwt = JWT.create() //
+                .withKeyId(privateKeyId) //
+                .withIssuer(clientEmail) //
+                .withSubject(clientEmail) //
+                .withAudience("https://www.googleapis.com/oauth2/v4/token") //
+                .withClaim("scope", "https://www.googleapis.com/auth/drive") //
+                .withIssuedAt(new Date(now)) //
+                .withExpiresAt(new Date(now + 3600 * 1000L)) //
+                .sign(Algorithm.RSA256(null, (RSAPrivateKey) privateKey));
+
+        final GenericUrl url = new GenericUrl("https://www.googleapis.com/oauth2/v4/token");
+        final GenericData data = new GenericData();
+        data.set("assertion", jwt);
+        data.set("grant_type", "urn:ietf:params:oauth:grant-type:jwt-bearer");
+        final HttpContent content = new UrlEncodedContent(data);
+        final HttpResponse response = new NetHttpTransport().createRequestFactory().buildPostRequest(url, content).execute();
+
+        final ObjectMapper mapper = new ObjectMapper();
+        final TokenResponse token = mapper.readValue(response.getContent(), TokenResponse.class);
+
+        return new Drive.Builder(new NetHttpTransport(), new JacksonFactory(),
+                request -> request.getHeaders().setAuthorization("Bearer " + token.getAccessToken())) //
+                .setApplicationName("Fess DataStore") //
+                .build();
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    static class TokenResponse {
+        @JsonProperty("access_token")
+        private String accessToken;
+        @JsonProperty("expires_in")
+        private Integer expiresIn;
+        @JsonProperty("token_type")
+        private String tokenType;
+        private String error;
+        @JsonProperty("error_description")
+        private String errorDescription;
+
+        public String getAccessToken() {
+            return accessToken;
+        }
+
+        public Integer getExpiresIn() {
+            return expiresIn;
+        }
+
+        public String getTokenType() {
+            return tokenType;
+        }
+
+        public String getError() {
+            return error;
+        }
+
+        public String getErrorDescription() {
+            return errorDescription;
+        }
     }
 
 }
--- src/test/java/org/codelibs/fess/ds/gsuite/GSuiteDataStoreTest.java
+++ src/test/java/org/codelibs/fess/ds/gsuite/GSuiteDataStoreTest.java
@@ -15,9 +15,16 @@
  */
 package org.codelibs.fess.ds.gsuite;
 
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.model.FileList;
 import org.codelibs.fess.ds.gsuite.GSuiteDataStore;
 import org.codelibs.fess.util.ComponentUtil;
 import org.dbflute.utflute.lastadi.ContainerTestCase;
+
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.spec.InvalidKeySpecException;
 
 public class GSuiteDataStoreTest extends ContainerTestCase {
 
@@ -45,9 +52,35 @@ public class GSuiteDataStoreTest extends ContainerTestCase {
         super.tearDown();
     }
 
-    public void test_xxx() {
-        // TODO
-        assertTrue(true);
+    public void testProduction() throws Exception {
+        // doDriveServiceTest();
+    }
+
+    public void testPrivateKey() {
+        final String privateKeyPem =
+                "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCn+4MuYt7wOw4O\nkm9TSlZioYZOcGMZWb6WC2OYZBUQ3XTsMNLfJtAp6CHPipwwWsl9wMgSR9dGeGiT\nmLnt3h3uGKa6gglzDyzMvJwrACx1VAG7fETV7nkeMYnph/S/smadnM/LIX/8szoX\nYG8IjE+L63tjGaamPP1EXTi9KJzTpf1bMPoK2K8oPsuimWYzEGLuSpoys5T4J8CH\nICIG6WlFrCayiMn3M+XWaWF1SpQsGqwtmXKwx0cGt855jY3RNncuhVUTaV1yav16\nwMjxcncpllAFUEBiPjcDqmTEDXAkjDk47436GWvC3ywgK9f6RJn6NyO6y4umDief\nh9bJcSqrAgMBAAECggEANWj5UTbbhotoUUbLAbMJm2ubHfaK88T+nYt6e7oanpWj\nNvPhIFWhaymlEm2ScqdzGDpDAtj3wC7JpWAygciVQJ5y6Ksu4PgKuQAhMWTpPdbb\nhCajZlfgB7Mdk/SH46mQCf4RbZc1r/22czdUpmWiZy0UurIts/6BUorHXxF6J6rD\nFtssYKbxt9gIwp/SZIIhwyGDDD4enayV3rV/o63nny2CXiIskJoWWFENOlUMQ3ZU\ntTt38dvaVIQaxAbNqbfpgAkP+gxeNCH/V65oucAsFg9SUz1DGVWtih74kX4YOZTl\nPgoCWudLI1g2r3JdcUY9Lu5d/s/cQ7rgjNUbh+hU9QKBgQDoRuTXTstiUykQMR60\ntVUi14IInrhi9FHvPXUjAiHzysfG/7LRrZU5BodzQUQdFEEgv/krSQUQZIhOln23\nfM6KoBvMQyOWX2XLbnW32wD9WkEVEQ2dD+gnRQEAoFmrp3oyQVCS2LG1Chgh0nxk\nRIgpgPsRWZjNIBKfoTRGMPSrbwKBgQC5I5PQFF/6LxkUJ72NEQwOIGl2zgKLOp7A\n0LygtnCYruniIecbE6wRIxAl7Pyitn3pLBrpOfkQ5/+62x/izi97SMPFhwDlSNDz\nl57Xz31HMMuXMwtkUfbWempVUtPazZagkv/UR03gcqv/2LOpQTYtZHWM9Nwket/q\ngKS01OiGhQKBgQDlxlPUxgaeMsPZV64Xd5ZLzEK/QjwG78gp0XMR+acao0uziPkd\nQQjwC4xssHCwD3xJ6H6VmjtfNwQ8MdUVcWpkafR1wSjwNVjewFvFT+mPtvvQ2jjM\nWZL/XdybAQUTsK5KDqClU6WgAOdUdgNFsSG9ZPF4/VgR+xtRPEgY847mEwKBgFjZ\ntip6XMVYrRR7LgDxWXO+Il8d5yKic2Xdx2jppYfhCeW4l4zrF/zxcuKApY4BPtQU\nkqWzaNCwRo6KcvcKNMdZ2HqHm+pt07nT3it8LIdp54scuC7hFzE/dqCUK5AqckuF\nwLlDryA0aE9U1IQ6A6ItQCzxpmTrg2KrcmBGfYFxAoGAfs5ND0bt3DkTpzq0SFej\nibdUv3Ida6rIj+uo78SKP/wjG2eD/mEl0PXT9JQcsEerFh5cdG9UXI182v1YmWSE\nxrwPtlnL//EpDTzRhnDGOy6pReo1jfrYw6PCPiaFbqvN89jgGk7dgdW31TJlOxQP\nAni2P2NjSP8YiS7B6FPYVS8=\n-----END PRIVATE KEY-----\n";
+        try {
+            final PrivateKey privateKey = GSuiteDataStore.getPrivateKey(privateKeyPem);
+        } catch (final Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+    protected void doDriveServiceTest() throws Exception {
+        final String projectId = "";
+        final String privateKeyPem = "";
+        final String privateKeyId = "";
+        final String clientEmail = "";
+        final PrivateKey privateKey = GSuiteDataStore.getPrivateKey(privateKeyPem);
+
+        try {
+            final Drive drive = GSuiteDataStore.getDriveService(projectId, privateKey, privateKeyId, clientEmail);
+            drive.files().list().execute().getFiles().forEach(f -> {
+                System.out.println(f.getName());
+            });
+        } catch (final IOException e) {
+            fail(e.getMessage());
+        }
     }
 
 }
