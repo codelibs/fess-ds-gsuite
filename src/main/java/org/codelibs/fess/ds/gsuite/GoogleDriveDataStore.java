d4dcc045cbbbc53d5abfb86115c92e7eb972ee72
Shinsuke Sugaya <shinsuke@apache.org>
2022-02-26 06:16:44 +0900

    fess 14.0.0

--- src/main/java/org/codelibs/fess/ds/gsuite/GoogleDriveDataStore.java
+++ src/main/java/org/codelibs/fess/ds/gsuite/GoogleDriveDataStore.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2021 CodeLibs Project and the Others.
+ * Copyright 2012-2022 CodeLibs Project and the Others.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -444,7 +444,8 @@ public class GoogleDriveDataStore extends AbstractDataStore {
         }
         if ("user".equals(type)) {
             return ComponentUtil.getSystemHelper().getSearchRoleByUser(value);
-        } else if ("group".equals(type) || "domain".equals(type)) {
+        }
+        if ("group".equals(type) || "domain".equals(type)) {
             return ComponentUtil.getSystemHelper().getSearchRoleByGroup(value);
         } else if ("anyone".equals(type)) {
             return ComponentUtil.getSystemHelper().getSearchRoleByUser("guest");
